660debec1c5aa4489512b3f713c80a0d7f0bede9
satinderpal singh <satindersandhu96@gmail.com>
2020-02-17 11:33:34 +1300

    comment

--- Movie Rental System/Program.cs
+++ Movie Rental System/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Movie_Rental_System
+namespace Movie_Rental_System// name of project
 {
     static class Program
     {
